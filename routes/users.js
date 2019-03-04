2debee52da089c9fc8bab177966c415f943b1ca0
ghehd <ghehd231@naver.com>
2019-03-04 21:00:02 +0900

    login.ejs rigister.ejs

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', (req, res, next) => {
-    res.send('routes/users.js => login');
+    //res.send('routes/users.js => login');
+    res.render('login');
 });
 
 router.get('/register', (req, res, next) => {
-    res.send('routes/users.js => register');
+   // res.send('routes/users.js => register');
+   res.render('register');
 });
 
 module.exports = router;
