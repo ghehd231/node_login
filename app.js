9c756fe8e6839ecf1ab9cf79269e411b6092d946
ghehd231 <ghehd231@naver.com>
2019-03-20 19:03:42 +0900

    mongo db connect

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const app = express();
+
+//db
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db,{useNewUrlParser: true})
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err))
 
 app.use(expressLayout);
 app.set('view engine','ejs');
+//express body parser
+app.use(express.urlencoded({extended:true}));
 
 app.use("/", require('./routes/index'));
 app.use("/users", require('./routes/users'));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI :'mongodb://ghehd231:rldjrdl2@ds047772.mlab.com:47772/node_login'
+}
