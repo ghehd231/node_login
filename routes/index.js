e2c9f9599593128a45c6e392453b67ec0dcb791e
ghehd231 <ghehd231@naver.com>
2019-03-22 20:47:23 +0900

    ensureAuthenticated

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const {ensureAuthenticated} = require('../config/auth');
 router.get('/', (req, res, next) => {
     //res.send('routes/index.js => home');
     res.render('welcome');
 });
 
-router.get('/dashboard', (req, res, next) => {
+router.get('/dashboard', ensureAuthenticated, (req, res, next) => {
     res.render('dashboard',{
         name: req.user.name
     });
